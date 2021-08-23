b117ba7a03e8c896d0776d3e11e7d60c9e706bc3
ilker-zeybek <ilker.zeybek.dev@gmail.com>
2021-08-23 21:06:21 +0300

    supabase read and insert users

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ const path = require('path');
 const cors = require('cors');
 const bcrypt = require('bcrypt');
 const supabase = require('./supabase');
-const { reset } = require('nodemon');
 
 const app = express();
 const port = 3000;
