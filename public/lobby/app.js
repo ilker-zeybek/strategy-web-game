c4cdff1afdec02839b9a201d955ed757a7f372e8
ilker-zeybek <ilker.zeybek.dev@gmail.com>
2021-09-08 21:08:52 +0300

    lobby implementation started

--- public/lobby/app.js
+++ public/lobby/app.js
@@ -1,3 +1,22 @@
-const app = {};
+const app = {
+  data() {
+    return {
+      rooms: [],
+    };
+  },
+  methods: {
+    goToProfile() {
+      window.location.href = 'http://localhost:3000/user/profile';
+    },
+    async signOut() {
+      let response = await fetch('http://localhost:3000/auth/signout');
+      response = await response.json();
+      message = response.message;
+      if (message === 'Successfully signed out.') {
+        window.location.href = 'http://localhost:3000/';
+      }
+    },
+  },
+};
 
 Vue.createApp(app).mount('#app');
